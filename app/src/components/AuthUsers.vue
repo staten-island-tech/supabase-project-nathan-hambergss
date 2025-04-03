11818a8bc2b21013ec3bdbba2e23b87274282deb
hambergss <144480774+hambergss@users.noreply.github.com>
2025-04-03 14:00:41 -0400

    users component

---
+++ app/src/components/AuthUsers.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup>
+import { supabase } from '../../supabase';
+
+const { data, error } = await supabase
+  .from('characters')
+  .select()
+</script>
+
+<style scoped>
+
+</style>
