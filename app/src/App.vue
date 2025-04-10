e2db849983343e999cb1fed5e6cdb821a5a2822a
Nathan k <nkweon@gmail.com>
2025-04-10 13:53:57 -0400

    1

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-
+    <h2>Welcome to the Anime App</h2>
+    <p>This is the homepage.</p>
+    <router-link to="/AniList">Go to Anime List</router-link>
   </div>
 </template>
 
-<script setup>
-
+<script>
+export default {
+  name: 'Home',
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
