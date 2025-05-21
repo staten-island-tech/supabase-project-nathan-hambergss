1cc8f46adb89ce8c20cfa3d77655dc9501db3995
hambergss <144480774+hambergss@users.noreply.github.com>
2025-05-21 14:00:41 -0400

    more daisyui

--- app/src/views/LoginView.vue
+++ app/src/views/LoginView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen bg-base-200 p-6 space-y-20">
-    <h1 class="text-2xl font-bold text-secondary">Login View</h1>
+  <div class="flex flex-col items-center justify-center min-h-screen bg-[#ffebff] p-6 space-y-20">
+    <h1 class="text-4xl font-bold text-secondary">Login View</h1>
     <SignIn />
   </div>
 </template>
--- app/src/views/SignupView.vue
+++ app/src/views/SignupView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen bg-base-200 p-6 space-y-20">
-    <h1 class="text-2xl font-bold text-primary">Sign Up</h1>
+    <h1 class="text-4xl font-bold text-primary">Sign Up</h1>
     <SignUp />
   </div>
 </template>
