1edfab6bf3025f5988cf9065b9ff71f03a0d6109
hambergss <144480774+hambergss@users.noreply.github.com>
2025-05-29 14:01:24 -0400

    remove components

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <h1 class="text-5xl font-bold text-primary">Welcome!</h1>
     <h1 class="text-4xl font-bold text-secondary">Please Sign Up/In to Access the Animes</h1>
     <div class="flex space-x-4">
-      <button class="btn btn-primary" @click="$router.push('/signup')">Go to Sign Up</button>
+      <button class="btn btn-outline btn-primary" @click="$router.push('/signup')">Go to Sign Up</button>
       <button class="btn btn-outline btn-secondary" @click="$router.push('/login')">
         Go to Sign In
       </button>
